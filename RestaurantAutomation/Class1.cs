942ca2fdc7fdb59bda74fc6cdeb63c89894af0e1
gentsapko <gentsapko@gmail.com>
2017-02-12 22:35:35 +0200

    Branch_Master_Page_With_Header,_Footer_and_Body

---
+++ RestaurantAutomation/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace RestaurantAutomation
+{
+    public class Class1
+    {
+        public void Go()
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://restaurant-12.apphb.com/");
+        }
+    }
+}
